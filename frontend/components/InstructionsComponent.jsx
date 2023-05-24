18a2ff81e48da0f743e4ccf43990374540574f6d
Michael <tanmichael858@gmail.com>
2023-05-23 21:41:36 -0400

    added input button for delegate and vote

--- frontend/components/InstructionsComponent.jsx
+++ frontend/components/InstructionsComponent.jsx
@@ -4,175 +4,229 @@ import { useSigner, useNetwork, useBalance } from 'wagmi';
 import { useState, useEffect } from 'react';
 
 export default function InstructionsComponent() {
-	const router = useRouter();
-	return (
-		<div className={styles.container}>
-			<header className={styles.header_container}>
-				<h1>
-					My dApper
-				</h1>
-			</header>
 
-			<div className={styles.buttons_container}>
-				<PageBody></PageBody>
-			</div>
-			<div className={styles.footer}>
-				Foooter
-			</div>
-		</div>
-	);
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <header className={styles.header_container}>
+        <h1>
+          My dApper
+        </h1>
+      </header>
+
+      <div className={styles.buttons_container}>
+        <PageBody></PageBody>
+      </div>
+      <div className={styles.footer}>
+        Foooter
+      </div>
+    </div>
+  );
 }
 
 function PageBody() {
-	return(
-		<div>
-			<WalletInfo></WalletInfo>
-			<ApiInfo></ApiInfo>
+  return (
+    <div>
+      <WalletInfo></WalletInfo>
+      <ApiInfo></ApiInfo>
       <RequestTokens></RequestTokens>
       <DelegateVotes />
       <Vote />
-
-		</div>
-	)
+      <ViewProposals />
+    </div>
+  )
 }
 
 function WalletInfo() {
-	const { data: signer, isError, isLoading } = useSigner();
-	const { chain, chains } = useNetwork();
-	if (signer) return (
-		<>
-			<p>Your account address is {signer._address}</p>
-			<p>Connected to the {chain.name} network</p>
-			<button onClick={() => signMessage(signer, "I love fried potatoes")}>Sign</button>
-			<WalletBalance></WalletBalance>
-		</>
-	)
-	if (isLoading) return (
-		<>
-			<p>Wait a while, the wallet is loading</p>
-		</>
-	)
-	return (
-		<>
-			<p>Connect a wallet</p>
-		</>
-	)
+  const { data: signer, isError, isLoading } = useSigner();
+  const { chain, chains } = useNetwork();
+  if (signer) return (
+    <>
+      <p>Your account address is {signer._address}</p>
+      <p>Connected to the {chain.name} network</p>
+      <button onClick={() => signMessage(signer, "I love fried potatoes")}>Sign</button>
+      <WalletBalance></WalletBalance>
+    </>
+  )
+  if (isLoading) return (
+    <>
+      <p>Wait a while, the wallet is loading</p>
+    </>
+  )
+  return (
+    <>
+      <p>Connect a wallet</p>
+    </>
+  )
 }
 
 function WalletBalance() {
-	const { data: signer } = useSigner();
-	const { data, isError, isLoading } = useBalance({
-		address: signer._address,
-	})
+  const { data: signer } = useSigner();
+  const { data, isError, isLoading } = useBalance({
+    address: signer._address,
+  })
 
-	if (isLoading) return <div>Fetching balance...</div>
-	if (isError) return <div>error fetching balance</div>
-	return (
-		<div>
-			Balance: {data?.formatted} {data?.symbol}
-		</div>
-		)
+  if (isLoading) return <div>Fetching balance...</div>
+  if (isError) return <div>error fetching balance</div>
+  return (
+    <div>
+      Balance: {data?.formatted} {data?.symbol}
+    </div>
+  )
 }
 
 function signMessage(signer, message) {
-	signer.signMessage(message).then(
-		(signature) => {console.log(signature)},
-		(error) => {console.log(error)})
+  signer.signMessage(message).then(
+    (signature) => { console.log(signature) },
+    (error) => { console.log(error) })
 }
 
 function ApiInfo() {
-	const [data, setData] = useState(null);
-	const [isLoading, setLoading] = useState(false);
-   
-	useEffect(() => {
-	  setLoading(true);
-	  fetch('https://random-data-api.com/api/v2/users')
-		.then((res) => res.json())
-		.then((data) => {
-		  setData(data);
-		  setLoading(false);
-		});
-	}, []);
-   
-	if (isLoading) return <p>Loading...</p>;
-	if (!data) return <p>No profile data</p>;
-   
-	return (
-	  <div>
-		<h1>{data.username}</h1>
-		<p>{data.email}</p>
-	  </div>
-	);
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://random-data-api.com/api/v2/users')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
+  return (
+    <div>
+      <h1>{data.username}</h1>
+      <p>{data.email}</p>
+    </div>
+  );
 }
 
 function RequestTokens() {
-	const { data: signer } = useSigner();
-	const [txData, setTxData] = useState(null);
-	const [isLoading, setLoading] = useState(false);
-	if (txData) return (
-		<div>
-			<p>Transaction completed!</p>
-			<a href={"https://mumbai.polygonscan.com/tx/" + txData.hash} target="_blank">{txData.hash}</a>
-		</div>
-	)
-	if (isLoading) return <p>Requesting tokens to be minted...</p>;
-	return (
-		<div>
-			<button onClick={() => requestTokens(signer, "0.01", setLoading, setTxData)}>
-				Request 1.0 MTK Token</button>
-		</div>
-	)
+  const { data: signer } = useSigner();
+  const [txData, setTxData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  if (txData) return (
+    <div>
+      <p>Transaction completed!</p>
+      <a href={"https://mumbai.polygonscan.com/tx/" + txData.hash} target="_blank">{txData.hash}</a>
+    </div>
+  )
+  if (isLoading) return <p>Requesting tokens to be minted...</p>;
+  return (
+    <div>
+      <button onClick={() => requestTokens(signer, "0.01", setLoading, setTxData)}>
+        Request 1.0 MTK Token</button>
+    </div>
+  )
 }
 
 function requestTokens(signer, amount, setLoading, setTxData) {
-	setLoading(true);
-	const requestOptions = {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-		body: JSON.stringify({ address: signer._address, amount: amount })
-	};
-	fetch('http://localhost:3001/request-tokens', requestOptions)
-		.then(response => response.json())
-		.then((data) => {
-			setTxData(data);
-			setLoading(true);
-	});
+  setLoading(true);
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ address: signer._address, amount: amount })
+  };
+  fetch('http://localhost:3001/request-tokens', requestOptions)
+    .then(response => response.json())
+    .then((data) => {
+      setTxData(data);
+      setLoading(true);
+    });
 }
 
 function DelegateVotes() {
-	const { data: signer } = useSigner();
-	const [txData, setTxData] = useState(null);
-	const [isLoading, setLoading] = useState(false);
-	if (txData) return (
-		<div>
-			<p>Transaction completed!</p>
-			<a href={"https://mumbai.polygonscan.com/tx/" + txData.hash} target="_blank">{txData.hash}</a>
-		</div>
-	)
-	if (isLoading) return <p>Requesting tokens to be minted...</p>;
-	return (
+  const { data: signer } = useSigner();
+  const [txData, setTxData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [delegateInput, setDelegateInput] = useState();
+  if (txData) return (
     <div>
-			<button onClick={() => delegateVotes(signer, "0.01", setLoading, setTxData)}>
-				Delegate Votes</button>
-		</div>
-	)
+      <p>Transaction completed!</p>
+      <a href={"https://mumbai.polygonscan.com/tx/" + txData.hash} target="_blank">{txData.hash}</a>
+    </div>
+  )
+  if (isLoading) return <p>Requesting tokens to be minted...</p>;
+  return (
+    <div>
+      <input placeholder="input vote" onChange={(e) => setDelegateInput(e.target.value)}></input>
+      <button onClick={() =>
+        delegateVotes(signer, delegateInput, setLoading, setTxData)}>
+        Delegate Votes</button>
+    </div>
+  )
+}
+
+function delegateVotes(setLoading, setTxData) {
+  setLoading(true);
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ address: signer._address })
+  };
+  fetch('http://localhost:3000/delegatevote', requestOptions)
+    .then(response => response.json())
+    .then((data) => {
+      setTxData(data); setLoading(true);
+    });
 }
 
 function Vote() {
-	const { data: signer } = useSigner();
-	const [txData, setTxData] = useState(null);
-	const [isLoading, setLoading] = useState(false);
-	if (txData) return (
-		<div>
-			<p>Transaction completed!</p>
-			<a href={"https://mumbai.polygonscan.com/tx/" + txData.hash} target="_blank">{txData.hash}</a>
-		</div>
-	)
-	if (isLoading) return <p>Requesting tokens to be minted...</p>;
-	return (
+  const { data: signer } = useSigner();
+  const [txData, setTxData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [voteInput, setVoteInput] = useState();
+  if (txData) return (
     <div>
-			<button onClick={() => Vote(signer, "0.01", setLoading, setTxData)}>
-				Vote on Proposal</button>
-		</div>
-	)
+      <p>Transaction completed!</p>
+      <a href={"https://mumbai.polygonscan.com/tx/" + txData.hash} target="_blank">{txData.hash}</a>
+    </div>
+  )
+  if (isLoading) return <p>Requesting tokens to be minted...</p>;
+  return (
+    <div>
+      <input placeholder="input vote" onChange={(e) => setVoteInput(e.target.value)}></input>
+      <button onClick={() =>
+        vote(signer, voteInput, setLoading, setTxData)}>
+        Vote on Proposal</button>
+    </div>
+  )
+}
+
+function vote(setLoading, setTxData) {
+  setLoading(true);
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ address: signer._address })
+  };
+  fetch('http://localhost:3000/vote', requestOptions)
+    .then(response => response.json())
+    .then((data) => {
+      setTxData(data); setLoading(true);
+    });
+}
+
+function ViewProposals() {
+  const { data: signer } = useSigner();
+  const { data, isError, isLoading } =
+  // Add logic to view propsals
+    {}
+
+  if (isLoading) return <div>Fetching Proposals...</div>
+  if (isError) return <div>error fetching proposals</div>
+  return (
+    <div>
+      Proposals: {data?.formatted}
+    </div>
+  )
 }
